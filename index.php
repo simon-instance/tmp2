1f08aff7062672731d75c54f623976b0fe18d269
simon <simonpeters05@gmail.com>
2022-04-14 10:15:08 +0000

    Fixed database

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
 <?php
 
-phpinfo();
+class Database {
+    private const DB_LOGIN = "applicatie";
+    private const DB_HOST = "iproj_db";
+    private const DB_DATABASE = "Movies";
+
+    private static $verbinding = PDO::class;
+
+    public static function init() {
+        $wachtwoord = rtrim(file_get_contents("/run/secrets/password_rdbms_app", true));
+        if (!$wachtwoord) {
+            throw new RuntimeException("Kon een wachtwoordbestand voor SQL Server niet uitlezen.");
+        }
+
+        $driver_options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        ];
+
+        self::$verbinding = new PDO("sqlsrv:Server=" . self::DB_HOST . ";Database=" . self::DB_DATABASE . ";ConnectionPooling=0;", self::DB_LOGIN, $wachtwoord, $driver_options);
+        // Bewaar het wachtwoord niet langer onnodig in het geheugen van PHP.
+        unset($wachtwoord);
+    }
+
+    public function __get($prop) {
+        if($prop === "conn") return self::$verbinding;
+    }
+}
+
+Database::init();
